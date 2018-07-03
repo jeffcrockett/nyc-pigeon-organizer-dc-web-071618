56ec421c15e78c948b71499dd05451e4e597add0
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-03 18:36:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ organized_pigeons = {}
+ data.each do |attribute, hash|
+  hash.each do |key, value_array|
+    value_array.each do |name|
+      organized_pigeons[name] = {:color => [], :gender => [], :lives => []}
+    end
+  end
+ end
+
+ 
+ data.each do |attribute, hash|
+   hash.each do |key, value_array|
+     organized_pigeons.keys.each do |name|
+      if value_array.include?(name)
+        organized_pigeons[name][attribute] << key.to_s
+      end
+     end
+   end
+ end
+  # binding.pry
+ organized_pigeons
 end
+
