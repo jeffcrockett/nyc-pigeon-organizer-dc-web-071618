878d7d6834fd694218abd525fb1e73e34bf6c91e
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-03 18:25:19 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
  data.each do |attribute, hash|
   hash.each do |key, value_array|
     value_array.each do |name|
-      organized_pigeons[name] = {}
+      organized_pigeons[name] = {:color => [], :gender => [], :lives => []}
     end
   end
  end
