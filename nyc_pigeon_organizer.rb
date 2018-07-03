bcc2f30ea1718197bceb6763a57505983a1e52cb
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-03 18:21:46 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ organized_pigeons = {}
+ data.each do |attribute, hash|
+  hash.each do |key, value_array|
+    value_array.each do |name|
+      organized_pigeons[name] = {}
+    end
+  end
+ end
 end
 
 ```ruby
@@ -19,6 +26,48 @@ pigeon_data = {
     "Central Park" => ["Alex", "Ms. K", "Lucky"],
     "Library" => ["Peter Jr."],
     "City Hall" => ["Andrew"]
+  }
+}
+```
+```
+Your output should look something like the hash below:
+
+ruby
+pigeon_list = {
+  "Theo" => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Subway"]
+  },
+  "Peter Jr." => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Library"]
+  },
+  "Lucky" => {
+    :color => ["purple"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
+  },
+  "Ms. K" => {
+    :color => ["grey", "white"],
+    :gender => ["female"],
+    :lives => ["Central Park"]
+  },
+  "Queenie" => {
+    :color => ["white", "brown"],
+    :gender => ["female"],
+    :lives => ["Subway"]
+  },
+  "Andrew" => {
+    :color => ["white"],
+    :gender => ["male"],
+    :lives => ["City Hall"]
+  },
+  "Alex" => {
+    :color => ["white", "brown"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
   }
 }
 ```
